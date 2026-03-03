469c95da05cc99d9504e14ec8f87ce01afc60aca
cheuyin <yinstanleycheung@gmail.com>
2026-03-03 13:22:30 -0800

    Remove dist/ folder

--- dist/main.js
+++
@@ -1,6 +0,0 @@
-import HangmanDrawing from "./hangman-drawing.js";
-import WordManager from "./word-manager.js";
-const hangmanDrawing = new HangmanDrawing();
-hangmanDrawing.drawInitial();
-new WordManager();
-//# sourceMappingURL=main.js.map
