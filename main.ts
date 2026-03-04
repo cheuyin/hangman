540dc82b529ebda4dfcd423ab013d505e0ef2687
cheuyin <yinstanleycheung@gmail.com>
2026-03-04 09:18:20 -0800

    Write rudimentary game loop

--- main.ts
+++ main.ts
@@ -1,7 +1,50 @@
+import promptSync from "prompt-sync";
 import HangmanDrawing from "./hangman-drawing.js";
 import WordManager from "./word-manager.js";
 
-const hangmanDrawing = new HangmanDrawing();
-hangmanDrawing.drawInitial();
+const prompt = promptSync();
 
-new WordManager();
+function runGame() {
+  // Introductory Message
+  console.log("Let's Play Hangman!");
+
+  const hangmanDrawing = new HangmanDrawing();
+  const wordManager = new WordManager();
+  const currentHangmanStep = 0;
+
+  const gameRunning = true;
+
+  // LOOP:
+  while (gameRunning) {
+    // Draw game states
+    hangmanDrawing.draw(currentHangmanStep);
+    console.log("Wrong Guesses: ", wordManager.getWrongGuesses());
+    console.log("Correct Guesses: ", wordManager.getCorrectGuesses());
+
+    // Get guess
+    const guess = getGuess();
+    // Update guesses
+    wordManager.processGuess(guess);
+    // Check if game is over
+    // Repeat
+  }
+}
+
+runGame();
+
+// Gets console input from user
+// Only accepts alphabetical characters
+// Returns the lower case of that character
+function getGuess(): string {
+  while (true) {
+    const name = prompt("Enter your guess: ");
+    const lettersOnly = /^[a-zA-Z]+$/;
+    if (name.length !== 1) {
+      console.log("Your guess should be one letter. Please try again.");
+    } else if (!lettersOnly.test(name)) {
+      console.log("Your guess should be a letter in the alphabet");
+    } else {
+      return name.toLowerCase();
+    }
+  }
+}
--- word-manager.ts
+++ word-manager.ts
@@ -1,14 +1,14 @@
 import { readFileSync } from "fs";
 
 export default class WordManager {
-  private secretWord: string[]; // Array of characters in the word
+  private secretWord: string[]; // Array of characters in the word, all lowercase
   private wrongGuesses: string[];
   private correctGuesses: string[];
 
   constructor() {
     this.secretWord = this.chooseRandomWord().split("");
     this.wrongGuesses = [];
-    this.correctGuesses = Array(this.secretWord.length).fill("");
+    this.correctGuesses = Array(this.secretWord.length).fill("_");
   }
 
   public chooseRandomWord(): string {
@@ -29,9 +29,35 @@ export default class WordManager {
     const randomIndex = Math.floor(Math.random() * numWords);
 
     // Choose that word
-    const randomWord = content[randomIndex] as string;
+    const randomWord = content[randomIndex]?.toLowerCase() as string;
 
     return randomWord;
+  }
+
+  // Takes a guessed letter from the user
+  // Updates the game state based on this guess
+  // Returns the array of correct guesses
+  public processGuess(guess: string) {
+    const formattedGuess = guess.toLocaleLowerCase();
+    if (this.secretWord.includes(formattedGuess)) {
+      // Update correctGuesses
+      const indexes = [];
+      let i = -1;
+      while ((i = this.secretWord.indexOf(formattedGuess, i + 1)) !== -1) {
+        indexes.push(i);
+      }
+      for (let idx of indexes) {
+        this.correctGuesses[idx] = guess;
+      }
+    } else {
+      this.wrongGuesses.push(guess);
+    }
+  }
+
+  // Returns true if all characters of secret word has been successfully guessed
+  // False otherwise
+  public checkAllLettersGuessed(): boolean {
+    return !this.correctGuesses.includes("_");
   }
 
   public getSecretWord(): string[] {
@@ -44,5 +70,15 @@ export default class WordManager {
 
   public getWrongGuesses(): string[] {
     return this.wrongGuesses;
+  }
+
+  public printWrongGuesses(): void {
+    const formattedString = this.wrongGuesses.join("  ");
+    console.log("Wrong Guesses: " + formattedString);
+  }
+
+  public printCorrectGuessses(): void {
+    const formattedString = this.correctGuesses.join("  ");
+    console.log("Correct Guesses: " + formattedString);
   }
 }
